c82d7e42fc36487530d003056f5e6d683a66d3e1
Perberto07 <johnpatrickboleche@gmail.com>
2024-05-10 02:04:10 +0800

    up

--- frontend/src/component/Addcustomer.jsx
+++ frontend/src/component/Addcustomer.jsx
@@ -1,37 +1,38 @@
 import './css/content.css'
 import Sidebar from "../Sidebar"
+import './css/addcustomer.css'
 
 function Addcustomer() {
   return (
     <><Sidebar />
       <div className="Content">
-        <div>
+        <div className='Content2'>
 
-        <div className='mb-2'>
-          <label htmlFor="username">Customer Name: </label>
-          <input
-            type="text"
-            id="username"
-            placeholder='Enter Customer Name'
-          />
-        </div>
+          <div className='one'>
+            <label htmlFor="username">Customer Name: </label>
+            <input
+              type="text"
+              id="username"
+              placeholder='Enter Customer Name'
+            />
+          </div>
 
-
-        <div className='mb-2'>
-          <label htmlFor="username">Enter Address: </label>
-          <input
-            type="text"
-            id="username"
-            placeholder='Enter Address'/>
-        </div>
-
-        <div className='mb-2'>
-          <label htmlFor="username">Store Name: </label>
-          <input
-            type="text"
-            id="username"
-            placeholder='Enter Store Name'/>
-        </div>
+          <hr />
+          <div className='two'>
+            <label htmlFor="username">Enter Address: </label>
+            <input
+              type="text"
+              id="username"
+              placeholder='Enter Address' />
+          </div>
+          <hr />
+          <div className='three'>
+            <label htmlFor="username">Store Name: </label>
+            <input
+              type="text"
+              id="username"
+              placeholder='Enter Store Name' />
+          </div>
 
         </div>
       </div>
