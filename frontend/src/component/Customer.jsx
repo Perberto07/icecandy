072083a0b926d6b2386615a11c0b776ba0edc677
loeydriyan <165555422+loeydriyan@users.noreply.github.com>
2024-05-15 14:04:22 +0800

    customer css

--- frontend/src/component/Customer.jsx
+++ frontend/src/component/Customer.jsx
@@ -1,41 +1,39 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/customer.css'; // Make sure to import your CSS file
-import './css/content.css'
+import './css/content.css';
 import Sidebar from '../Sidebar';
 
 const CustomerComponent = () => {
   return (
     <>
-    <Sidebar/>
-    <div className='Content'>
-    <div className="container">
-      <div className='customer'>
-      <ul><li><h2>
-        <Link to="/Customer/Customerlist" className="button">
-          <i className="icon fas fa-info-circle"></i>Customer
-        </Link>
-      </h2></li>
-     
-      
-        <li>
-          <Link to="/Customer/Addcustomer" className="button">
-            <i className="icon fas fa-plus"></i>Add Customer
-          </Link>
-        </li>
-        <li>
-          <Link to="/Customer/Editcustomer" className="button">
-            <i className="icon fas fa-handshake"></i>Edit Customer
-          </Link>
-        </li>
-        <li>
-          <Link to="/Customer/Deletecustomer" className="button">
-            <i className="icon fas fa-handshake"></i>Delete Customer
-          </Link>
-        </li>
-      </ul>
+      <Sidebar />
+      <div className='Content'>
+        <div className="container">
+          <div className='customer'>
+            <div className="link-container">
+              <Link to="/Customer/Customerlist" className="button">
+                <i className="icon fas fa-info-circle"></i>Customer
+              </Link>
+            </div>
+            <div className="link-container">
+              <Link to="/Customer/Addcustomer" className="button">
+                <i className="icon fas fa-plus"></i>Add Customer
+              </Link>
+            </div>
+            <div className="link-container">
+              <Link to="/Customer/Editcustomer" className="button">
+                <i className="icon fas fa-handshake"></i>Edit Customer
+              </Link>
+            </div>
+            <div className="link-container">
+              <Link to="/Customer/Deletecustomer" className="button">
+                <i className="icon fas fa-handshake"></i>Delete Customer
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </>
   );
 };
