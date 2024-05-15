73ad97ee9f79a0812dfd37820f3dca2b27c0bc03
Perberto07 <johnpatrickboleche@gmail.com>
2024-05-15 14:01:14 +0800

    up

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,27 +1,23 @@
 import { Link } from "react-router-dom";
-import './component/css/Sidebar.css'
+import './component/css/Sidebar.css';
 import Header from "./Header";
-
 
 function Sidebar() {
   return (
     <>
-    
       <Header/>
-        <div className="sidebar">
-          <nav>
-            <ul>
-              <li><Link to="/Home" className="active"> <i className="fas fa-home"></i> Home</Link></li>
-              <li><Link to="/Customer"> <i className="fas fa-info-circle"></i> Customer</Link></li>
-              <li><Link to="/Product"> <i className="fas fa-plus"></i> Product</Link></li>
-              <li><Link to="/Order"> <i className="fas fa-handshake"></i> Order</Link></li>
-              <li><Link to="/Transaction"> <i className="fas fa-money-bill"></i> Transaction</Link></li>
-              <li><Link to="/Transaction/Settings"> <i className="fas fa-cog"></i> Settings</Link></li>
-              <li><Link to="/"> <i className="fas fa-sign-out-alt"></i> Logout</Link></li>
-            </ul>
-          </nav>
-        </div>
-        
+      <div className="sidebar">
+        <nav>
+          <ul>
+            <li><Link to="/Home" className="active"> <i className="bi bi-house-door"></i> Home</Link></li>
+            <li><Link to="/Customer"> <i className="bi bi-info-circle"></i> Customer</Link></li>
+            <li><Link to="/Product"> <i className="bi bi-plus-circle"></i> Product</Link></li>
+            <li><Link to="/Order"> <i className="bi bi-handshake"></i> Order</Link></li>
+            <li><Link to="/Transaction"> <i className="bi bi-cash-stack"></i> Transaction</Link></li>
+            <li><Link to="/"> <i className="bi bi-box-arrow-right"></i> Logout</Link></li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
