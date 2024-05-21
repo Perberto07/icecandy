0b1cf8690cb9783ce1f4e623a433d6f2c1751cc0
Perberto07 <johnpatrickboleche@gmail.com>
2024-05-21 18:07:56 +0800

    up

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import './component/css/Sidebar.css';
 import Header from "./Header";
+import Home from './images/home.png';
+import Customer from './images/customer.png';
+import Product from './images/product.png';
+import Order from './images/order.png';
+import Transaction from './images/transaction.png';
+import Logout from './images/logout.png';
 
 function Sidebar() {
   return (
@@ -9,12 +15,12 @@ function Sidebar() {
       <div className="sidebar">
         <nav>
           <ul>
-            <li><Link to="/Home" className="active"> Home</Link></li>
-            <li><Link to="/Customer"> Customer</Link></li>
-            <li><Link to="/Product">Product</Link></li>
-            <li><Link to="/Order"> Order</Link></li>
-            <li><Link to="/Transaction">Transaction</Link></li>
-            <li><Link to="/">Logout</Link></li>
+            <li><Link to="/Home" className="active"><img src={Home} alt="Home" className="Home" /> Home</Link></li>
+            <li><Link to="/Customer"><img src={Customer} alt="customer" className="Customer" /> Customer</Link></li>
+            <li><Link to="/Product"><img src={Product} alt="product" className="Product" />Product</Link></li>
+            <li><Link to="/Order"><img src={Order} alt="order" className="Order" /> Order</Link></li>
+            <li><Link to="/Transaction"><img src={Transaction} alt="Transaction" className="Transaction" />Transaction</Link></li>
+            <li><Link to="/"><img src={Logout} alt="Logout" className="Logout" />Logout</Link></li>
           </ul>
         </nav>
       </div>
